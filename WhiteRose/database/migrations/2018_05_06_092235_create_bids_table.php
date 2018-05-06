1ea7a124b2bc1ea9a35dcad46c35897e231746d7
nikolap123 <nikolap@elfak.rs>
2018-05-06 20:11:08 +0200

    -This commit will push bids migration

---
+++ WhiteRose/database/migrations/2018_05_06_092235_create_bids_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('amount');
+            $table->integer('job_id');
+            $table->integer('client_id');
+            $table->integer('pentester_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
