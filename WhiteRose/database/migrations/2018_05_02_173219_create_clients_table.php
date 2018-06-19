d6a5060704cf08fd48e8662fe6457cdcb6c24ea9
nikolap123 <nikolap@elfak.rs>
2018-06-19 23:41:01 +0200

    -this commit will fix clients table

---
+++ WhiteRose/database/migrations/2018_05_02_173219_create_clients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('username')->nullable();
+            $table->string('lastname');
+            $table->integer('tokens');
+            $table->string('confirmation_code')->nullable();
+            $table->integer('confirmed')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+    }
+}
